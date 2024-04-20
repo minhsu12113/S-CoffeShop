51c945fbc956bb9cbbdb0bcc9fae8f8bcffdc6cd
justin <minhsu12113@gmail.com>
2024-04-20 14:47:00 +0700

    first commit

---
+++ CoffeShop/Viewmodel/Categorys/CateroryAddOrUpdateViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Business.Model;
+using CoffeShop.ExtentionCommon;
+using CoffeShop.Model;
+using CoffeShop.Utility;
+using CoffeShop.View.Dialog;
+using CoffeShop.Viewmodel.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CoffeShop.Viewmodel.Categorys
+{
+    public class CateroryAddOrUpdateViewModel : BindableBase
+    {
+        #region [Variable]
+        private CategoryModel _currentCategory;
+        private int _maxLengthName;
+        public bool _isEdit { get; set; }
+
+
+        public int MaxLengthName
+        {
+            get { return _maxLengthName; }
+            set { _maxLengthName = value; OnPropertyChanged(); }
+        }
+        public CategoryModel CurrentCategory
+        {
+            get { return _currentCategory; }
+            set { _currentCategory = value; OnPropertyChanged(); }
+        }
+        #endregion
+        #region [Action]
+        public Action ReLoadList { get; set; }
+        public Action CloseDialogParent { get; set; }
+        #endregion
+        #region [Command]
+        public ICommand CloseDialogCMD { get { return new CommandHelper(CloseDialogParent); } }
+        public ICommand SaveCMD { get { return new CommandHelper(Save); } }
+        #endregion
+
+        public CateroryAddOrUpdateViewModel(Action reLoadListCat, Action closeDialog, CategoryModel category = null)
+        {
+            if (category == null) // Add new
+            {
+                CurrentCategory = new CategoryModel(); 
+            }                
+            else  // Edit
+            {
+                CurrentCategory = category.Clone();
+                _isEdit = true;
+            }
+               
+
+            MaxLengthName = CurrentCategory.GetAttributeFrom<MaxLengthAttribute>(nameof(CurrentCategory.Name)).Length;
+            ReLoadList = reLoadListCat;
+            CloseDialogParent = closeDialog;
+        }
+        public void Save()
+        {
+             
+        }
+    }
+}
---
+++ CoffeShop/Viewmodel/InitViewmodel.cs
@@ -0,0 +1,60 @@
+﻿using CoffeShop.DBHelper;
+using CoffeShop.Enums;
+using CoffeShop.Internationalization;
+using CoffeShop.Utility;
+using CoffeShop.View;
+using CoffeShop.Viewmodel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CoffeShop.Viewmodel
+{
+    public class InitViewmodel : BindableBase
+    {
+        public InitViewmodel()
+        {
+            StringResources.ApplyLanguage(ALL_ENUM.LANGUAGE.VN);
+            Loaded();
+        }
+        public async void Loaded()
+        {
+            await Task.Run(() =>
+            {
+            try
+            {
+                Thread.Sleep(3000);
+                string connectString = DBSetting.Instance.LoadConfig();
+                bool isConnectDB = DBSetting.Instance.CheckConnectMainDB(connectString);
+
+                CSGlobal.Instance.LoginWindow = new LoginWindow();
+                CSGlobal.Instance.LoginWindow.Show();
+                CSGlobal.Instance.InitializeWindow.Hide();
+                CSGlobal.Instance.MainWindow = new MainWindow();
+                CSGlobal.Instance.MainWindow.Show();
+ 
+               }
+               catch (Exception ex)
+               {
+
+
+               }
+           });
+        }
+        public async void CreateAccountAdmin()
+        {
+            await Task.Run(() =>
+            {
+                //using (var unitOfWork = new UnitOfWork(new CoffeeShopContext()))
+                //{
+                //    unitOfWork.User.CheckAndCreateAccountAdmin();
+                //}
+            });
+        }        
+    }
+}
